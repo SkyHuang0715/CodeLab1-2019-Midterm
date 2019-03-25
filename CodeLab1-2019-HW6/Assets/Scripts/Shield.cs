6f258d561150430eb868d9cb8240e97b7c3f6b6e
SkyHuang0715 <she515996344@gmail.com>
2019-03-25 14:48:02 -0400

    In class code

---
+++ CodeLab1-2019-HW6/Assets/Scripts/Shield.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public float duration = 100;
+
+    public float damageMod = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float ReduceDamage(float damage)
+    {
+        return damage = damageMod;
+        
+    }
+}
